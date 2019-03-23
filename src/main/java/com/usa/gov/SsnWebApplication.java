5ee7865953b36754686c4c25e0ebfd2429ed1b1b
sodamreddy <pullareddy970@gmail.com>
2019-03-23 20:21:47 +0530

    first commit

---
+++ src/main/java/com/usa/gov/SsnWebApplication.java
@@ -0,0 +1,13 @@
+package com.usa.gov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsnWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsnWebApplication.class, args);
+	}
+
+}
