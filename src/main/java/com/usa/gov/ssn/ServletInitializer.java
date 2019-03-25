d4a72908b97984acf2c008587e39ff843a899d50
sodamreddy <pullareddy970@gmail.com>
2019-03-25 21:23:57 +0530

    local

--- src/main/java/com/usa/gov/ssn/ServletInitializer.java
+++ src/main/java/com/usa/gov/ssn/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.usa.gov;
+package com.usa.gov.ssn;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/com/usa/gov/ssn/SsnWebApplication.java
+++ src/main/java/com/usa/gov/ssn/SsnWebApplication.java
@@ -1,4 +1,4 @@
-package com.usa.gov;
+package com.usa.gov.ssn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
